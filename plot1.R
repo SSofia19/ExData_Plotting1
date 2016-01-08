b62f1febc09ceac402410287bf0307870602278f
SSofia19 <sarajsofia@gmail.com>
2016-01-08 17:49:13 -0500

    first commit

--- plot1.R
+++ plot1.R
@@ -1,5 +1,15 @@
 
+file <- "~/exdata-data-household_power_consumption/household_power_consumption.txt"
 
-household<- read.table("./data/household_power_consumption.txt", header=F, skip=66637, nrow=2880, sep=";")
-colnames(household) <- c("Date", "Time", "Global_active_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-hist(household$Global_active_power, main= "Global Active Power", xlab= "Global Active Power (kilowatts)", col="red")
+
+household <- read.table(file, header=TRUE, sep=";", na.strings = "?")
+household$Date <- as.Date(household$Date, format="%d/%m/%Y")
+
+hh<- household[(household$Date == "2007-02-01") | (household$Date == "2007-02-02"),]
+
+hh$Global_active_power <- as.numeric(as.character(hh$Global_active_power))
+
+hist(hh$Global_active_power, col="red", main =paste("Global Active Power"), xlab="Global Active Power(kilowatts)" )
+
+dev.copy(png, file="plot1.png")
+dev.off()
